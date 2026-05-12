d929ba2de87f46a9a7e990c9215ee4d8c4de4cc9
saichandrapandraju <saichandrapandraju@gmail.com>
2026-05-12 10:26:05 -0400

    Chore: Remove obsolete project files and configurations.

--- .konflux/prep-hermeto.sh
+++
@@ -1,4 +0,0 @@
-cp ../requirements-x86_64.txt ppc64le/requirements.txt
-cp ../requirements-x86_64.txt s390x/requirements.txt
-cp ../requirements-build.txt ppc64le/requirements-build.txt
-cp ../requirements-build.txt s390x/requirements-build.txt
